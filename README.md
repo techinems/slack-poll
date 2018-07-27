031f3e643bd201cb9b2221954727dc5305e243e1
Logan Ramos <lramos15@gmail.com>
2018-07-26 20:10:07 -0700

    Added Slack App

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # slack-poll
 A simple databaseless polling app for Slack
+
+## How to Setup
+1. Create a Slack App with slash command and chat:write bot scope
+2. Add a slash command of /inorout and an interactive message and provide your server's url + /slack/commands if it's the command url and /slack/actions if it is the interactive messages url in the required fields on slack's website.
+3. Fill out the .env file with the necessary tokens.
+4. npm start
+5. You're done!
+
+## How to Use
+/inorout Question
+:emoji: Answer
+:emoji2: Answer2
+etc.
+
+Emojis are optional but always fun :)
---
+++ server.js
@@ -0,0 +1,167 @@
+const env = require('dotenv').config()
+const request = require('request');
+const {createMessageAdapter} = require('@slack/interactive-messages');
+const { WebClient } = require('@slack/client');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const smb = require('slack-message-builder')
+
+//If the environment variables don't get loaded
+if(env.error){
+    throw env.error
+}
+
+const slackVerificationToken = process.env.SLACK_VERIFICATION_TOKEN;
+const slackAccessToken = process.env.SLACK_ACCESS_TOKEN;
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+
+const slackInteractions = createMessageAdapter(slackVerificationToken);
+const web = new WebClient(slackAccessToken);
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/slack/actions', slackInteractions.expressMiddleware());
+app.post('/slack/commands',slackSlashCommands);
+
+slackInteractions.action({callbackId: 'answer', type: 'button'}, (payload,respond) =>{
+
+    // We have to avoid the Slack Message Builder package when working with pre-existing messages because it's just wrong...
+    var clickedButton = payload.original_message.attachments[0].actions[(payload.actions[0].value.split(',')[0] - 1)];
+
+    // This ensures that the user has only voted on one of the options.
+    for(let i = 0; i < payload.original_message.attachments[0].actions.length; i++){
+        let currentAction = payload.original_message.attachments[0].actions[i];
+        let currentValue = new Set(currentAction.value.split(','));
+        if(currentValue.has(payload.user.id)){
+            currentValue.delete(payload.user.id);
+            currentAction.value = Array.from(currentValue).join(',');
+        }
+    }
+
+    var newButtonValue = clickedButton.value + ',' + payload.user.id;
+    // We don't want multiple of the same people to have voted
+    newButtonValue = new Set(newButtonValue.split(','));
+    clickedButton.value = Array.from(newButtonValue).join(',');
+
+    var resultString = generateResultString(payload);
+
+    var currentMessage = smb(payload.original_message);
+    // If the person is the first voter
+    if (payload.original_message.attachments.length == 2){
+        currentMessage.attachment().text(resultString).color('#32CD32').end();
+    }else{
+        currentMessage.attachments.get(-1).text(resultString).color('#32CD32').end();
+    }
+    return currentMessage.json();
+});
+
+slackInteractions.action({callbackId: 'options', type: 'button'}, (payload,respond) =>{
+    console.log(payload);
+    if (payload.actions[0].name == 'deletepoll')
+    {
+        if (payload.user.id == payload.actions[0].value){
+            return "This poll has been deleted.";
+        }else{
+            web.chat.postEphemeral({channel: payload.channel.id, text: "Only the poll author may delete the poll.", user:payload.user.id});
+        }
+    }else if (payload.actions[0].name == 'resetvote'){
+        
+        for(let i = 0; i < payload.original_message.attachments[0].actions.length; i++){
+            let currentAction = payload.original_message.attachments[0].actions[i];
+            let currentValue = new Set(currentAction.value.split(','));
+            if(currentValue.has(payload.user.id)){
+                currentValue.delete(payload.user.id);
+                currentAction.value = Array.from(currentValue).join(',');
+            }
+        }
+
+        var resultString = generateResultString(payload);
+    
+        var currentMessage = smb(payload.original_message);
+        // If the person is the first voter
+        if (payload.original_message.attachments.length == 2){
+            currentMessage.attachment().text(resultString).color('#32CD32').end();
+        }else{
+            currentMessage.attachments.get(-1).text(resultString).color('#32CD32').end();
+        }
+        return currentMessage.json();
+    }else if (payload.actions[0].name == 'movepoll'){
+        var currentMessage = smb(payload.original_message);
+        web.chat.delete({channel: payload.channel.id, ts: payload.message_ts});
+        web.chat.postMessage({channel:payload.channel.id, text: payload.original_message.text, attachments: payload.original_message.attachments});
+    }
+});
+
+function generateResultString(payload){
+
+    var resultString = "";
+    for(let i = 0; i < payload.original_message.attachments[0].actions.length; i++){
+        let currentAction = payload.original_message.attachments[0].actions[i];
+        // Don't need a set here because we already removed the repeats.
+        let currentValue = currentAction.value.split(',');
+        let numberOfVotes = currentValue.length - 1;
+        if(numberOfVotes == 0){
+            continue;
+        }
+        resultString += currentAction.text + " (" + numberOfVotes + ")" + " =>";
+        for (let x = 1; x < currentValue.length; x++){
+            resultString += " " + "<@" + currentValue[x] + ">,";
+        }
+        
+        if(resultString.slice(-1) == ','){
+            resultString = resultString.slice(0,-1);
+        }
+    
+        resultString += "\n";
+    }
+    resultString = strip(resultString);
+    return resultString;
+}
+
+const port = process.env.PORT || 3000;
+
+http.createServer(app).listen(port,() =>{
+    console.log(`Server listening on port ${port}`);
+});
+
+function slackSlashCommands(req,res,next){
+    /* This is all to verify that the command is coming from slack
+    var timeStamp  = req.header('X-Slack-Request-Timestamp');
+    var sigBaseString = 'v0:' + timeStamp + ':' + JSON.stringify(req.body);
+    var hmachHash = crypto.createHmac('sha256', slackSigningSecret).update('v0=' + sigBaseString).digest('hex');
+    var slackSignature = req.header('X-Slack-Signature');*/
+    // If the command came from slack (this is susceptible to a timing attack, but it's just a slack polling extension...)
+    if(req.body.token == slackVerificationToken){
+
+        if(req.body.command === '/inorout'){
+            var pollParameters = req.body.text.split('\n');
+            var slackMessage = smb().responseType('in_channel');
+            var attachmentArray = slackMessage.text(pollParameters[0]).attachment().callbackId('answer')
+            for(var i = 1; i < pollParameters.length; i++){
+                attachmentArray.button().name('answer').text(pollParameters[i]).type('button').value(i).end()
+            }
+            var optionsButtons = slackMessage.attachment().callbackId('options').color('#0000FF');
+            optionsButtons.button().text("Reset vote").name('resetvote').type('button').end();
+            optionsButtons.button().text("Move to Bottom").name('movepoll').type('button').end();
+            optionsButtons.button().text("Delete Poll").name('deletepoll').type('button').style('danger').value(req.body.user_id).end();
+            slackMessage.json();
+            request({
+                url: req.body.response_url,
+                method: "POST",
+                json: true,
+                body: slackMessage
+            });
+            res.send();
+        }
+    }else{
+        console.log("Comparison failed.");
+        next();
+    }
+}
+
+// Removes the whitespace from the end of strings
+function strip(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+}
