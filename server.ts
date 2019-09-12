10f3e68b081e9c7ac76093fe568db211b53fb983
Logan Ramos <lramos15@gmail.com>
2019-09-12 13:18:06 -0400

    Add some slack node packages

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { urlencoded } from 'body-parser';
-import { stringify } from 'qs';
-import { createHmac, timingSafeEqual } from 'crypto';
+import { createMessageAdapter } from '@slack/interactive-messages';
 import { Poll } from './Poll';
 
 // Load Environment variables
@@ -20,34 +19,12 @@ if (!(slackAccessToken && slackSigningSecret)) {
 
 // Intialize Express app
 const app = express();
-app.use(urlencoded({ extended: true }));
 
-app.use('/slack/commands', (req, res, next) => {
-    const slackSignature = req.headers['x-slack-signature'] as string;
-    const requestBody = stringify(req.body, { format: 'RFC1738' });
-    const timestamp = req.headers['x-slack-request-timestamp'] as string;
-    // Verify slack headers were received
-    if (!(timestamp && requestBody && slackSignature)) {
-        return res.send('Failed to receive proper headers')
-    }
-    // convert current time from milliseconds to seconds
-    const time = Math.floor(new Date().getTime() / 1000);
-    // Most likely a replay attack
-    if (Math.abs(time - parseInt(timestamp)) > 300) {
-        return res.send('Ignore this request.');
-    }
-    let sigBasestring = 'v0:' + timestamp + ':' + requestBody;
-    let mySignature = 'v0=' +
-        createHmac('sha256', slackSigningSecret)
-            .update(sigBasestring, 'utf8')
-            .digest('hex');
-    // Safe against timing attacks
-    if (timingSafeEqual(Buffer.from(mySignature, 'utf8'), Buffer.from(slackSignature, 'utf8'))) {
-        next();
-    } else {
-        return res.status(200).send('Request verification failed');
-    }
-});
+// Ensure messages come from slack
+const slackInteractions = createMessageAdapter(slackSigningSecret);
+app.use('/slack/commands', slackInteractions.requestListener());
+
+app.use(urlencoded({ extended: true }));
 
 app.post('/slack/commands', (req, res) => {
     if (req.body.command === "/inorout") {
