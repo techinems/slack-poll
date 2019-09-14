2b10e8872d3bf13482fd5bd609a30f0a09ee68b0
Logan Ramos <lramos15@gmail.com>
2019-09-14 11:24:12 -0400

    Initial Poll message building

--- Poll.ts
+++ Poll.ts
@@ -1,5 +1,76 @@
-export class Poll {
-    constructor(author: string, parameters: string[]) {
+import { KnownBlock, SectionBlock, ContextBlock, KnownAction, Button, ActionsBlock, StaticSelect } from "@slack/types";
 
+export class Poll {
+
+    private message: KnownBlock[] = [];
+    constructor(author: string, parameters: string[]) {
+        const titleBlock: SectionBlock = {
+            type: 'section',
+            text: { type: 'mrkdwn', text: parameters[0] }
+        };
+        const authorBlock: ContextBlock = {
+            type: 'context',
+            elements: [
+                { type: 'mrkdwn', text: `Asked by: ${author}` }
+            ]
+        }
+        this.message.push(titleBlock);
+        this.message.push(authorBlock);
+        const actionBlocks: ActionsBlock[] = [{ type: 'actions', elements: [] }];
+        let actionBlockCount: number = 0;
+        // Construct all the buttons
+        for (let i = 1; i < parameters.length; i++) {
+            if (i % 5 === 0) {
+                const newActionBlock: ActionsBlock = { type: 'actions', elements: [] };
+                actionBlocks.push(newActionBlock);
+                actionBlockCount++;
+            }
+            const button: Button = { type: 'button', action_id: 'answer', text: { type: 'plain_text', text: parameters[i], emoji: true } };
+            actionBlocks[actionBlockCount].elements.push(button);
+        }
+        // The various poll options
+        const selection: StaticSelect = {
+            type: 'static_select',
+            placeholder: {
+                type: 'plain_text',
+                text: 'Poll Options',
+                emoji: true
+            },
+            options: [
+                {
+                    text: {
+                        type: 'plain_text',
+                        text: 'Reset your vote',
+                        emoji: true
+                    },
+                    value: 'reset'
+                },
+                {
+                    text: {
+                        type: 'plain_text',
+                        text: 'Move to bottom',
+                        emoji: true
+                    },
+                    value: 'bottom'
+                },
+                {
+                    text: {
+                        type: 'plain_text',
+                        text: 'Delete poll',
+                        emoji: true
+                    },
+                    value: 'delete'
+                }
+            ]
+        };
+        actionBlockCount++;
+        actionBlocks.push({ type: 'actions', elements: [selection] });
+        this.message.concat(actionBlocks);
+        // Add a divider in between so later we can put the messages
+        this.message.push({ type: 'divider' });
+    }
+
+    public getBlocks() {
+        return this.message;
     }
 }
--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import { urlencoded } from 'body-parser';
 import { createMessageAdapter } from '@slack/interactive-messages';
+import { WebClient } from '@slack/web-api';
 import { Poll } from './Poll';
 
 // Load Environment variables
@@ -20,6 +21,9 @@ if (!(slackAccessToken && slackSigningSecret)) {
 // Intialize Express app
 const app = express();
 
+// Intialize Slack Web client for sending requests
+const webclient = new WebClient(slackAccessToken);
+
 // Ensure messages come from slack
 const slackInteractions = createMessageAdapter(slackSigningSecret);
 app.use('/slack/commands', slackInteractions.requestListener());
@@ -30,6 +34,7 @@ app.post('/slack/commands', (req, res) => {
     if (req.body.command === "/inorout") {
         // Create a new poll passing in the poll author and the other params
         const poll = new Poll(`<@${req.body.user_id}>`, req.body.text.split('\n'));
+        webclient.chat.postMessage({ channel: req.body.channel_id, text: 'Unable to render block content.', as_user: false, blocks: poll.getBlocks() })
     } else {
         console.error(`Unregistered command ${req.body.command}`);
         res.send('Unhandled command');
