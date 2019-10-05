2a931f15d223e8303cf38b5adf13a96bdcde1669
Matthew Peveler <matt.peveler@gmail.com>
2019-10-05 07:35:06 -0300

    fix error message

--- src/Actions.ts
+++ src/Actions.ts
@@ -76,7 +76,7 @@ export class Actions {
             res.sendStatus(200);
         } catch (err) {
             console.error(err);
-            res.send(`Something went wrong: ${err}`);
+            res.send(`Something went wrong`);
         }
     }
 
