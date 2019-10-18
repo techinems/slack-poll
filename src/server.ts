63243085395647ae63641bc17304d8f3811c0384
Justin Etzine <justetz@gmail.com>
2019-10-17 23:59:50 -0700

    additional sentry features

--- src/server.ts
+++ src/server.ts
@@ -3,34 +3,36 @@ import * as dotenv from "dotenv";
 import { urlencoded } from "body-parser";
 import { createMessageAdapter } from "@slack/interactive-messages";
 import { Actions } from "./Actions";
-import { Sentry } = require('@sentry/node');
-
-// Configure Sentry exception logging
-if (process.env.SENTRY_DSN) {
-    Sentry.init({ dsn: process.env.SENTRY_DSN });
-}
+import * as Sentry from '@sentry/node';
 
 // Load Environment variables
 dotenv.config();
 
-// Load in the environment variables
-const SLACK_ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN;
-const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
-
-const PORT = process.env.PORT || 3000;
-
-if (!(SLACK_ACCESS_TOKEN && SLACK_SIGNING_SECRET)) {
+// Verify required env varables are set
+if (!process.env.SLACK_ACCESS_TOKEN || !process.env.SLACK_SIGNING_SECRET) {
     throw "Environment variables not properly loaded!";
 }
+
+// Configure Sentry exception logging
+if (process.env.SENTRY_DSN) {
+    const sentryConfig : Sentry.NodeOptions = {
+        dsn: process.env.SENTRY_DSN,
+        release: require('../package.json').version
+    };
+    if (process.env.ENVIRONMENT) sentryConfig.environment = process.env.ENVIRONMENT;
+    Sentry.init(sentryConfig);
+}
+
+const PORT = process.env.PORT || 3000;
 
 // Intialize Express app
 const app = express();
 
 // Intialize Slack Web client for sending requests
-const actions = new Actions(SLACK_ACCESS_TOKEN);
+const actions = new Actions(process.env.SLACK_ACCESS_TOKEN);
 
 // Ensure messages come from slack
-const slackInteractions = createMessageAdapter(SLACK_SIGNING_SECRET);
+const slackInteractions = createMessageAdapter(process.env.SLACK_SIGNING_SECRET);
 app.use("/slack/actions", slackInteractions.expressMiddleware());
 
 app.use(urlencoded({ extended: true }));
